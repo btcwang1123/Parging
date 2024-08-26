6451faf4fb07fffe03c3e5d3c75444084625e9ef
btcwang1123 <78584500+btcwang1123@users.noreply.github.com>
2024-08-26 13:50:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def main():
                     地址: {park['ADDRESS']}<br>
                     平日收費: {park['WEEKDAYS']}<br>
                     假日收費: {park['HOLIDAY']}<br>
-                    <a href="https://www.google.com/maps/dir/?api=1&destination={park['LATITUDE']},{park['LONGITUDE']}" target="_blank">
+                    <a href="https://www.google.com/maps/dir/?api=1&destination={park['LATITUDE']},{park['LONGITUDE']}" target="_blank" style="text-align: center;">
                         <img src="https://png.pngtree.com/png-clipart/20230917/original/pngtree-volkswagen-beetle-car-sticker-retro-clipart-vector-png-image_12270600.png" alt="導航" style="width:50px;height:50px;"><br>導航
                     </a>
                     </div>
