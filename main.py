bc8e7c4dd2c1f8e04f38a601bb21cbafcd8ade70
btcwang1123 <78584500+btcwang1123@users.noreply.github.com>
2024-08-26 11:06:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def main():
                     平日收費: {park['WEEKDAYS']}<br>
                     假日收費: {park['HOLIDAY']}<br>
                     <a href="https://www.google.com/maps/dir/?api=1&destination={park['LATITUDE']},{park['LONGITUDE']}" target="_blank">
-                        <img src="img/car_icon.png" alt="導航" style="width:20px;height:20px;">
+                        <img src="https://png.pngtree.com/png-vector/20230822/ourlarge/pngtree-vw-beetle-in-the-sunset-vw-car-tattoo-illustration-clipart-vector-png-image_7026644.png" alt="導航" style="width:20px;height:20px;">
                     </a>
                     </div>
                 """, max_width=220),
